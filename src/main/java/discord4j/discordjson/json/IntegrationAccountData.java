2b0c9d0eca598f8ba27c3c363a73ea2a6e150e9a
Krakenied <Krakenied1@gmail.com>
2023-04-26 02:18:16 +0200

    Add IntegrationAccountData

---
+++ src/main/java/discord4j/discordjson/json/IntegrationAccountData.java
@@ -0,0 +1,21 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableIntegrationAccountData.class)
+@JsonDeserialize(as = ImmutableIntegrationAccountData.class)
+public interface IntegrationAccountData {
+
+    static ImmutableIntegrationAccountData.Builder builder() {
+        return ImmutableIntegrationAccountData.builder();
+    }
+
+    /** id of the account */
+    String id();
+
+    /** name of the account */
+    String name();
+}
