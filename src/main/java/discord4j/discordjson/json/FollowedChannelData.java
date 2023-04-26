bdbcb592c2994c04b2c05f95409c1c5c2ee5eb94
Krakenied <Krakenied1@gmail.com>
2023-04-26 05:09:46 +0200

    Add FollowedChannelData

---
+++ src/main/java/discord4j/discordjson/json/FollowedChannelData.java
@@ -0,0 +1,25 @@
+package discord4j.discordjson.json;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import discord4j.discordjson.Id;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableFollowedChannelData.class)
+@JsonDeserialize(as = ImmutableFollowedChannelData.class)
+public interface FollowedChannelData {
+
+    static ImmutableFollowedChannelData.Builder builder() {
+        return ImmutableFollowedChannelData.builder();
+    }
+
+    /** source channel id */
+    @JsonProperty("channel_id")
+    Id channelId();
+
+    /** created target webhook id */
+    @JsonProperty("webhook_id")
+    Id webhookId();
+}
